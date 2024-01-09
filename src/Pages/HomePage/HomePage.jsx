3414b2022496794b7d48d46f0f3f136c883c2049
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2024-01-09 23:12:11 +0300

    aws-export.js added

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 import { signIn } from "aws-amplify/auth";
 import { Amplify } from "aws-amplify";
-import awsmobile from "../../aws-exports";
+import awsmobile from "../../Authentication/aws-exports";
 import { signOut } from "aws-amplify/auth";
 import { fetchAuthSession } from "aws-amplify/auth";
 import { useNavigate } from "react-router-dom";
