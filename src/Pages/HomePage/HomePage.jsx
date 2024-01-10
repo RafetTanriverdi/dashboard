f8559ba042e8fc851c892d0e93fed71c7a199a06
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2024-01-10 20:41:23 +0300

    import error fixed

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,12 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import awsExports from "../../Authentication/aws-exports";
 
 import PublicLayout from "../../Layout/PublicLayout/PublicLayout";
 import RTHeader from "../../Components/RTHeader/RTHeader";
-import { Amplify } from "aws-amplify";
 
-Amplify.configure(awsExports);
 const HomePageContainer = () => {
   return (
     <>
