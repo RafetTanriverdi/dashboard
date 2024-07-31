cb311c872c9342acf112bc56a67902841c655e6a
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2024-07-31 23:44:32 +0300

    theme updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-
-
 import "./App.css";
 import AppClientRouter from "./AppClientRouter";
 import { routes } from "./routing/routes";
 import { useUserDataStore } from "./data/User/UserData";
-
 import { useThemeChangeStore } from "./data/Theme/Theme";
 import { ConfigProvider } from "antd";
 import { darkTheme } from "./theme/DarkTheme/DarkTheme";
@@ -18,40 +14,30 @@ import { useCookies } from "react-cookie";
 function App() {
   const { theme, setTheme } = useThemeChangeStore();
   const { setUserData } = useUserDataStore();
-
-
   const [cookies, setCookie] = useCookies(["light-theme"]);
-
- 
-
-
-  
 
   useEffect(() => {
     const isLightTheme = cookies["light-theme"] === "true";
-    setTheme(isLightTheme ?true : false); // Varsayılan olarak 'true' (light theme) yapıyoruz
-  themes.current(theme?"generic.light":"generic.dark");
-    const user = async () =>
-      await getCurrentUser()
-        .then((user) => {
-          setUserData(user);
-        })
-        .catch((err) => {
-          console.log("err", err);
-        });
-    user();
-  
-    setCookie("light-theme", theme);
-  }, [cookies, setTheme, setUserData, setCookie]);
-  
+    setTheme(isLightTheme !== undefined ? isLightTheme : true);
+    themes.current(isLightTheme ? "generic.light" : "generic.dark");
 
-  console.log("theme", theme);
+    const user = async () => {
+      try {
+        const currentUser = await getCurrentUser();
+        setUserData(currentUser);
+      } catch (err) {
+        console.log("Error fetching user data:", err);
+      }
+    };
+    user();
+
+    setCookie("light-theme", theme);
+  }, [cookies, setTheme, setUserData, setCookie, theme]);
+
   return (
-    <>
-      <ConfigProvider theme={theme ? lightTheme : darkTheme}>
-        <AppClientRouter routes={routes} />
-      </ConfigProvider>
-    </>
+    <ConfigProvider theme={theme ? lightTheme : darkTheme}>
+      <AppClientRouter routes={routes} />
+    </ConfigProvider>
   );
 }
 
--- src/components/RTHeader/RTHeader.jsx
+++ src/components/RTHeader/RTHeader.jsx
@@ -3,11 +3,18 @@ import { useThemeChangeStore } from "@rt/data/Theme/Theme";
 import { useUserDataStore } from "@rt/data/User/UserData";
 import { Button, Space } from "antd";
 import { Typography } from "antd";
+import themes from "devextreme/ui/themes";
 
 const RTHeader = () => {
   const { userData } = useUserDataStore();
   const { username } = userData;
-  const { theme, changeTheme } = useThemeChangeStore();
+  const { theme, setTheme } = useThemeChangeStore();
+
+  const changeTheme = () => {
+    const newTheme = !theme;
+    setTheme(newTheme);
+    themes.current(newTheme ? "generic.light" : "generic.dark");
+  };
 
   return (
     <>
@@ -15,7 +22,7 @@ const RTHeader = () => {
         <Typography.Title level={5}>Welcome {username}</Typography.Title>
         <Button
           type="link"
-          onClick={() => changeTheme()}
+          onClick={changeTheme}
           icon={theme ? <MoonOutlined /> : <SunOutlined />}
         />
       </Space>
