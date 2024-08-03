09d07801608ab086ae1e3d60adc2408d2062a27a
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2024-08-03 18:17:22 +0300

    ignore changed

--- src/authentication/auth-utils.js
+++ src/authentication/auth-utils.js
@@ -1,8 +1,9 @@
+import awsmobile from "@rt/aws-exports";
 
 
 
 const getToken = () => {
-  const poolIdFromEnv=import.meta.env.VITE_COGNITO_USER_POOL_ID
+  const poolIdFromEnv=awsmobile.aws_user_pools_web_client_id
   const userId=localStorage.getItem(`CognitoIdentityServiceProvider.${poolIdFromEnv}.LastAuthUser`);
   const token = localStorage.getItem(`CognitoIdentityServiceProvider.${poolIdFromEnv}.${userId}.accessToken`);
   return token;
