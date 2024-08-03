ada4407fa231c2b6d512d7c95a15412988bec2d9
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2024-08-03 14:52:28 +0300

    login process updated

--- src/components/RTSider/RTSider.jsx
+++ src/components/RTSider/RTSider.jsx
@@ -3,10 +3,10 @@ import { NavLink } from "react-router-dom";
 import { signOut } from "aws-amplify/auth";
 import { useNavigate } from "react-router-dom";
 import { Amplify } from "aws-amplify";
-import awsExports from "@rt/aws-exports";
 import { getRoutePath, routes } from "../../routing/routes";
 import { ROUTES_ID } from "../../routing/routes-id";
 import { useAuthStore } from "@rt/data/Auth/UseAuthStore";
+import awsExports from '@rt/aws-exports'
 
 
 
