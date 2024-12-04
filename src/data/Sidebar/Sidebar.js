9a83a100682f1dc2d86faec9762127a5c74ec89e
RafetTanriverdi <by.rafet.tanriverdi@gmail.com>
2024-12-04 15:22:47 +0300

    sidebar error

--- src/data/Sidebar/Sidebar.js
+++ src/data/Sidebar/Sidebar.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 export const useSidebarStore = create((set) => ({
-  isCollapsed: localStorage.getItem("collapse") === "true" || false, 
+  isCollapsed: localStorage.getItem("collapse") === "true" , 
   isMobile: window.innerWidth < 768,
   initialize: () => {
     if (typeof window !== "undefined") {
